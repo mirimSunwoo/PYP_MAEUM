8fe250c70149f3c27a0dcd4bf81fe434d45e8e8b
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-11-28 23:49:24 +0900

    해인이꺼 커밋하고 TOP3구성 완료

--- TOP3.py
+++ TOP3.py
@@ -1,15 +1,32 @@
-
 import tkinter
-import tkinter.font
 
-import START
 import MAEUM_MAIN
-
+import subprocess
 class Top3:
-    def __init__(self, top3):
-        self.top3 = top3
+    def __init__(self):
+        self.TD = TD
+        #화면 이미지
+        self.startBack = tkinter.PhotoImage(file = "img/top3_back.png")
+        self.startBackL = tkinter.Label(image=self.startBack)
+        self.startBackL.place(x=-2, y=-2)
+        self.homeBtn = tkinter.PhotoImage(file='img/back_yellow.PNG')
+        self.btn = tkinter.Button(TD, image=self.homeBtn, comand=None, borderwidth=0)
+        self.btn.pack()
+        self.btn.place(x=700, y=30)
+    def move(self):
+        start_move = MAEUM_MAIN.Maeum_main(self.start)
 
-        # share 화면 이미지
-        self.top3Back = tkinter.PhotoImage(file="img/background.gif")
-        self.top3BackL = tkinter.Label(image=self.top3Back)
-        self.top3BackL.place(x=-2, y=-2)
+    def play(self):
+        self.TD.mainloop()
+
+    def Button(self, TD, image, command):
+        pass
+
+if __name__ == '__main__':
+    TD = tkinter.Tk()
+    TD.title("Thank Share")
+    TD.geometry("882x628+250+250")
+    TD.resizable(False, False)
+
+    TD = Top3()
+    TD.play()
