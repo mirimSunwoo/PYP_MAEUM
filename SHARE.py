9bc4f1a65c4fb0f1fec9a5a016ca961fd23b6155
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-11-29 12:07:39 +0900

    share btn 가져가랏!

--- SHARE.py
+++ SHARE.py
@@ -3,21 +3,21 @@ import tkinter
 import MAEUM_MAIN
 import subprocess
 class SHARE:
-    def __init__(self):
+    def __init__(self,main):
         self.TD = TD
         #화면 이미지
         self.startBack = tkinter.PhotoImage(file = "img/share_back.png")
         self.startBackL = tkinter.Label(image=self.startBack)
         self.startBackL.place(x=-2, y=-2)
         self.homeBtn = tkinter.PhotoImage(file='img/back_yellow.PNG')
-        self.btn = tkinter.Button(TD, image=self.homeBtn, comand=None, borderwidth=0)
+        self.btn = tkinter.Button(TD, image=self.homeBtn, width = 120, height = 68, comand=None, borderwidth=0)
         self.btn.pack()
         self.btn.place(x=700, y=20)
 
-        self.Share = tkinter.PhotoImage(file='img/btn_share.png')
-        self.Sbtn = tkinter.Button(TD, image=self.Share, comand=None, borderwidth=0)
+        self.Share = tkinter.PhotoImage(file='img/share_btn.png')
+        self.Sbtn = tkinter.Button(TD, image=self.Share,width = 215, height = 64, comand=None, borderwidth=0, command=self.move)
         self.Sbtn.pack()
-        self.Sbtn.place(x=570, y=500)
+        self.Sbtn.place(x=590, y=510)
 
         self.ent1 = tkinter.Entry(TD)  # TD라는 창에 입력창 생성
         self.ent1.place(x=350,y=200)
@@ -34,7 +34,7 @@ class SHARE:
 
 
     def move(self):
-        start_move = MAEUM_MAIN.Maeum_main(self.start)
+        MAEUM_MAIN.Maeum_main(self.start)
 
     def play(self):
         self.TD.mainloop()
@@ -48,5 +48,5 @@ if __name__ == '__main__':
     TD.geometry("882x628+250+250")
     TD.resizable(False, False)
 
-    TD = SHARE()
+    TD = SHARE(TD)
     TD.play()
