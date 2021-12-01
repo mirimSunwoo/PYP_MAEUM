085f5f4eda23d8fc36dff80bfaedcd56153acb88
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-12-01 11:26:45 +0900

    share total, private maeums commit_2

--- SHARE.py
+++ SHARE.py
@@ -76,7 +76,7 @@ class Share:
         wall_label.mainloop()
         # label.mainloop()
 
-        NextButton = tkinter.Button(width=271, height=70,borderwidth=0, command=self.NextButton)
+        NextButton = tkinter.Button(width=271, height=70,borderwidth=0, command=self.NextButton)        
         NextButton.place(x=250, y=9)
         NextButtonImg = tkinter.PhotoImage(file="img/maenus_nextBtn.png")
         NextButton.config(image=NextButtonImg)
